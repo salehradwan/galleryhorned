59e2b9c1ac5431d3bd386ee3db651dcff9d3933d
salehradwan <saleh-radwan@outlook.com>
2021-06-08 10:58:20 +0300

    class-02 without bootstrap

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
 class HornedBeasts extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            counter: 0
+        }
+    }
+    increment = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
     render() {
         console.log(this.props);
         return (
             <main>
                 <div>
                     <h2> {this.props.title}</h2>
-                    <img src={this.props.img} alt= {this.props.title} />
+                    <img onClick={this.increment} src={this.props.img} alt= {this.props.title} />
                     <p>
                     {this.props.description}
                     </p>
+                    <h3> 😋 {this.state.counter}</h3>
                 </div>
             </main>
         )
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
+import hornedData from './Data.json';
 
 // class name which is a  (react class component)
 //  class Main is our react class component
@@ -7,29 +8,29 @@ import HornedBeasts from './HornedBeasts';
 // it inherite the ability to render things in the webpage
 class Main extends React.Component {
     render() {
-        const hornedBeasts = [{
-            "image_url": "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-            "title": "UniWhal",
-            "description": "A unicorn and a narwhal nuzzling their horns",
-            "keyword": "narwhal",
-            "horns": 1
-        },
+        // const hornedBeasts = [{
+        //     "image_url": "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
+        //     "title": "UniWhal",
+        //     "description": "A unicorn and a narwhal nuzzling their horns",
+        //     "keyword": "narwhal",
+        //     "horns": 1
+        // },
 
-        {
-            "image_url": "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80",
-            "title": "Rhino Family",
-            "description": "Mother (or father) rhino with two babies",
-            "keyword": "rhino",
-            "horns": 2
-        },
+        // {
+        //     "image_url": "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80",
+        //     "title": "Rhino Family",
+        //     "description": "Mother (or father) rhino with two babies",
+        //     "keyword": "rhino",
+        //     "horns": 2
+        // },
 
-        {
-            "image_url": "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg",
-            "title": "Unicorn Head",
-            "description": "Someone wearing a creepy unicorn head mask",
-            "keyword": "unicorn",
-            "horns": 1
-        }];
+        // {
+        //     "image_url": "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg",
+        //     "title": "Unicorn Head",
+        //     "description": "Someone wearing a creepy unicorn head mask",
+        //     "keyword": "unicorn",
+        //     "horns": 1
+        // }];
         return (
             // initalizing the horned beast component
             // creating an instance
@@ -61,7 +62,7 @@ class Main extends React.Component {
                  */}
 
                  {
-                     hornedBeasts.map((beast) => {
+                     hornedData.map((beast) => {
                          return (
                              <HornedBeasts 
                                 title = {beast.title}
