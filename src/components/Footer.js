f34353784761768e5ee4f8c894b4f97391d6adf9
salehradwan <saleh-radwan@outlook.com>
2021-06-08 16:28:01 +0300

    class-03 passing Functions in props

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
 
 class Footer extends React.Component {
     render () {
         const myName = 'Saleh Radwan';
         return (
-            <footer>
-                <span>
-                   Auther: {myName}
-                </span>
-            </footer>
+            // <footer>
+            //     <span>
+            //        Auther: {myName}
+            //     </span>
+            // </footer>
+
+            <>
+                <Navbar bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand>Auther: {myName}</Navbar.Brand>
+                    </Container>
+                </Navbar>
+
+            </>
         )
     }
 }
